cf8244275c8e0646629374445a98938994156ec5
Kait Hunnicutt <RenHunnicutt@Rens-MacBook-Air.local>
2021-02-19 17:17:13 -0700

    first submit

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,23 @@
 // Write your Character component here
+import React, {useState} from 'react'
+
+
+export default function CharacterInfo(props) {
+    const { characterName, close } = props
+    const [information, setInformation] = useState(null)
+
+
+return (
+    <div className = 'container'>
+        { <h2> {characterName} Information </h2>}
+    
+        <p> {characterName.gender} </p>
+        <p> {characterName.mass} </p>
+        <p> {characterName.height} </p>
+
+    
+    </div>
+)
+
+
+} 
